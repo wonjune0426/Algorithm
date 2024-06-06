c859c64ca81d5823afe7540c674a66bbe534282a
wonjune0426 <97424544+wonjune0426@users.noreply.github.com>
2024-06-06 15:30:39 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("\\    /\\");
+        bw.newLine();
+        bw.write(" )  ( ')");
+        bw.newLine();
+        bw.write("(  /  )");
+        bw.newLine();
+        bw.write(" \\(__)|");
+
+        bw.flush();
+        bw.close();
+    }
+}
