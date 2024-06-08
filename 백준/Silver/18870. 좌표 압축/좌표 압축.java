e5b875609e32dfbfc8af50df41791e52832d521e
wonjune0426 <97424544+wonjune0426@users.noreply.github.com>
2024-06-08 22:45:39 +0900

    [Silver II] Title: 좌표 압축, Time: 2712 ms, Memory: 228212 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -10,15 +10,18 @@ public class Main {
 
         StringTokenizer st;
 
+        // 입력되는 수의 갯수
         int N = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
 
+        // 초기 값을 저장하기 위한 배열
         int[] arr = new int[N];
 
+        // 정렬을 통해서 값을 변경하기 위한 TreeMap
         Map<Integer, Integer> map = new TreeMap<>();
 
-        int y = 0;
+        // 반복문을 통해 배열과 TreeMap에 저장 초기 값은 1로 둠
         for (int i = 0; i < N; i++) {
             int s = Integer.parseInt(st.nextToken());
             arr[i] = s;
@@ -28,12 +31,14 @@ public class Main {
 
         int cnt = 0;
 
+        // 정렬된 상태의 TreeMap에서 작은 값 0부터 1씩 늘려 value 변경
         for (Integer key : map.keySet()) {
             map.replace(key, cnt);
             cnt++;
         }
 
 
+        // 원본 배열과 map의 key를 비교하여 value 값으로 변경 후 출력
         for (int i = 0; i < N; i++) {
             if (map.containsKey(arr[i])) arr[i] = map.get(arr[i]);
 
