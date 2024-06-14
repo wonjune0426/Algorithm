855c846a3f9ba2ba1a3289a0166b61f3485ea437
wonjune0426 <97424544+wonjune0426@users.noreply.github.com>
2024-06-14 11:00:00 +0900

    [Bronze II] Title: 우유 축제, Time: 152 ms, Memory: 15996 KB -BaekjoonHub

--- 백준/Bronze/14720. 우유 축제/우유 축제.java
+++ 백준/Bronze/14720. 우유 축제/우유 축제.java
@@ -5,33 +5,35 @@ public class Main {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     private static StringTokenizer st;
-    
+
     public static void main(String[] args) throws IOException {
 
         // 우유 가게의 수
         int N = Integer.parseInt(br.readLine());
-        
+
         // 마실 우유를 확인할 변수
         int milk = 0;
 
         // 우유를 마시는 횟수를 저장
         int count = 0;
-        
+
         st = new StringTokenizer(br.readLine());
-        
+
+        // 가게 수 만큼 반복
         for(int i = 0; i < N; i++) {
+            // 우유 마실 순서와 가게가 일치하면 ++
            if(milk == Integer.parseInt(st.nextToken())) {
                count++;
-               if(milk == 2){
+               if(milk == 2){  // 2일 경우 0으로 순서 변환
                    milk = 0;
                } else{
                    milk++;
                }
            }
         }
-        
+
         br.close();
-        
+
         bw.write(count + "\n");
         bw.flush();
         bw.close();
