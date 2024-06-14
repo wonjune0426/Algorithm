016cfb9fec78a7b04dd7469d37627b6539389d58
wonjune0426 <97424544+wonjune0426@users.noreply.github.com>
2024-06-14 11:36:06 +0900

    [Silver III] Title: 등수 매기기, Time: 768 ms, Memory: 52572 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.java
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -10,20 +10,29 @@ public class Main {
         // 학생의 수
         int N = Integer.parseInt(br.readLine());
 
+        // 점수들을 저장할 배열
         int[] scores = new int[N];
 
+        // 각 점수를 입력 받음
         for (int i = 0; i < N; i++) {
             scores[i] = Integer.parseInt(br.readLine());
         }
 
+        // 불만도의 합을 구할 변수
         long sum = 0;
 
+        // 오름 차순 정렬 후 등수를 매기는 것이 제일 불만도가 적음
         Arrays.sort(scores);
 
+        // 각각의 불만도를 더함
         for (int i = 0; i < N; i++) {
             sum += Math.abs(scores[i] -(i+1));
         }
-
+        
+        // 출력 때문에 2번 틀림..
+//        bw.write(sum+"\n");
+//        bw.write(sum+"");
+        
         bw.write(String.valueOf(sum));
         bw.flush();
         bw.close();
