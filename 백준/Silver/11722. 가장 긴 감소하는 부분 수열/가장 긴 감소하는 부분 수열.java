602ae1e9cdd083bf26939e0a54f95264be88188d
wonjune0426 <97424544+wonjune0426@users.noreply.github.com>
2024-06-15 14:20:42 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 132 ms, Memory: 15300 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -13,6 +13,7 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         int[] arrN = new int[N];
+        // 수열의 길이를 저장할 배열
         int[] d = new int[N];
 
         st = new StringTokenizer(br.readLine());
@@ -22,20 +23,27 @@ public class Main {
             arrN[i] = Integer.parseInt(st.nextToken());
         }
 
+        // 초기값 1로 지정
         d[0] = 1;
+
+        // 수열 index 1번부터 반복
         for (int i = 1; i < N; i++) {
-            d[i] = 1;
-            for (int j = 0; j < i; j++) {
+            d[i] = 1; // 각 수열의 길이 1로 지정
+            for (int j = 0; j < i; j++) { // 해당 번호의 index까지 반복
+                // arrN[i] < arrN[j] -- 감소하는 수열
+                // d[i]의 값을 1증가
                 if (arrN[i] < arrN[j] && d[i] <= d[j]) {
                     d[i] = d[j] + 1;
-                } else if (arrN[i] == arrN[j]) {
+                } else if (arrN[i] == arrN[j]) { // 값이 같다면 같은 길이
                     d[i] = d[j];
                 }
             }
         }
 
         int max = 0;
-        for(int i:d){
+        
+        // 길이 배열의 값 중 max값 추출
+        for(int i:d){  
             max = Math.max(max,i);
         }
 
